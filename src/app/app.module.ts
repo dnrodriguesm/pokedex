14f208d46f480bff8f863ff4585881dd2c4f202e
Danilo Rodrigues <dnrodriguesm@MacBook-Pro-de-Danilo.local>
2020-02-13 20:23:04 -0300

    Limpando projeto inicial do Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {}
